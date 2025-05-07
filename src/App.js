28a2e20c51b92e67311b81f5941f34744dd57eb1
satyamthakurjs <epicsam399@gmail.com>
2025-05-07 11:52:32 +0800

    scaffold: src/App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div>this is my portfolio</div>;
-}
+const App = () => {
+  return <div>Hi</div>;
+};
 
 export default App;
